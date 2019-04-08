033fe57df4eb4e5fea070177f79a7a13f19d243f
alicek106 <alice_k106@naver.com>
2019-04-08 13:48:37 +0900

    Change README.md, set DETACTED service account key

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ As many people know, official Kubernetes python SDK is not kind. This repository
 
 Contributions are invited. Help me!
 
-이 소스코드는 2018년 6월 경에 작성된 소스코드를 다시 작성한 것입니다.
+이 소스코드는 2018년 6월 경에 작성된 소스코드를 이용해, SDK 사용 방법을 정리합니다.
---
+++ auth/auth_parser.py
@@ -0,0 +1,32 @@
+import configparser
+import os
+
+
+class AuthParser:
+    IN_CLUSTER_POD_AUTH = 1
+    SERVICE_ACCOUNT_TOKEN_AUTH = 2
+    CERTIFICATE_AUTH = 3
+
+    auth_type = None
+    service_account_token = None
+    cert_file_name = None
+    cert_key_file_name = None
+    kube_api_server_ip = None
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config/auth_config.ini'))
+        self.auth_type = int(config['AUTH_CONFIG']['AUTH_TYPE'])
+
+        if self.auth_type == self.SERVICE_ACCOUNT_TOKEN_AUTH:
+            self.service_account_token = config['AUTH_CONFIG']['SERVICE_ACCOUNT_TOKEN']
+            self.kube_api_server_ip = config['AUTH_CONFIG']['KUBE_API_SERVER_IP']
+        elif self.auth_type == self.CERTIFICATE_AUTH:
+            self.cert_file_name = config['AUTH_CONFIG']['CERT_FILE_NAME']
+            self.cert_key_file_name = config['AUTH_CONFIG']['CERT_KEY_FILE_NAME']
+            self.kube_api_server_ip = config['AUTH_CONFIG']['KUBE_API_SERVER_IP']
+        else:
+            pass
+
+    def get_auth_type(self):
+        return self.auth_type
