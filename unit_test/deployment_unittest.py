cafb255845c933453fc69e927e99bd9a95890b15
alicek106 <alice_k106@naver.com>
2019-06-14 14:45:26 +0900

    Add comment for custom api instance (issue #1)

--- unit_test/deployment_unittest.py
+++ unit_test/deployment_unittest.py
@@ -27,31 +27,32 @@ class DeplymentUnittest(unittest.TestCase):
                                                                                 namespace='default',
                                                                                 name='alicek106')
 
-        self.api_instance_appv1 = ApiInstanceGetter().get_appv1_api_instance(
+        self.api_instance_appv1_custom = ApiInstanceGetter().get_appv1_api_instance(
             custom_token_auth=bytes(secret_token.data['token'], 'utf-8'))
 
     def test_sample_deployment(self):
-        # List all deployments in namespace
-        deployments_all_ns = DeploymentExamples().list_deployment_all_namespace(self.api_instance_appv1)
-        self.assertGreater(len((deployments_all_ns.items)), 0)
+        # List all deployments in namespace -> If kubernetes 1.14+, below error occurs.
+        #  User \"system:serviceaccount:default:alicek106\" cannot list resource \"deployments\" in API group \"apps\" at the cluster scope"
+        # deployments_all_ns = DeploymentExamples().list_deployment_all_namespace(self.api_instance_appv1_custom)
+        # self.assertGreater(len((deployments_all_ns.items)), 0)
 
         # Create sample deployment
-        result, status = DeploymentExamples().create_deployment_sample(self.api_instance_appv1, DeploymentMessage('sample-deployment'))
+        result, status = DeploymentExamples().create_deployment_sample(self.api_instance_appv1_custom, DeploymentMessage('sample-deployment'))
         self.assertEqual(result['error_code'], 200)
 
         # Check deployment is created
-        deployment_ns = DeploymentExamples().list_deployment_namespace(self.api_instance_appv1, 'default')
+        deployment_ns = DeploymentExamples().list_deployment_namespace(self.api_instance_appv1_custom, 'default')
         self.assertGreater(len((deployment_ns.items)), 0)
 
         # Get deployment by metadata.name (selector)
-        deployment_by_selector = DeploymentExamples().list_deployment_namespace_by_selector(self.api_instance_appv1, 'default', 'metadata.name=sample-deployment')
+        deployment_by_selector = DeploymentExamples().list_deployment_namespace_by_selector(self.api_instance_appv1_custom, 'default', 'metadata.name=sample-deployment')
         self.assertEqual(len(deployment_by_selector.items), 1)
 
         # Get deployment by label
-        deployment_by_selector = DeploymentExamples().list_deployment_namespace_by_label(self.api_instance_appv1, 'default', 'alicek106_love_is_you=ls')
+        deployment_by_selector = DeploymentExamples().list_deployment_namespace_by_label(self.api_instance_appv1_custom, 'default', 'alicek106_love_is_you=ls')
         self.assertEqual(len(deployment_by_selector.items), 1)
 
-        result, status = DeploymentExamples().delete_deployment_sample(self.api_instance_appv1, DeploymentMessage('sample-deployment'))
+        result, status = DeploymentExamples().delete_deployment_sample(self.api_instance_appv1_custom, DeploymentMessage('sample-deployment'))
         self.assertEqual(result['error_code'], 200)
 
     def tearDown(self):
