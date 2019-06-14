08d6b44bcccc019fb041707cd96560fca7a31c52
alicek106 <alice_k106@naver.com>
2019-06-14 14:44:28 +0900

    Fix custom token auth using built-in environment variables for API server connection

--- auth/api_instance_getter.py
+++ auth/api_instance_getter.py
@@ -31,7 +31,12 @@ class ApiInstanceGetter:
 
     def __get_custom_token_auth(self, token):
         configuration = client.Configuration()
-        configuration.host = self.auth_parser.kube_api_server_ip
+        """
+        If you are developing in local IDE, you can use self.auth_parser.kube_api_server_ip for remote connect.
+        But if not, (If you deploy pod in kubernetes) just use Environment Variable :D
+        """
+        # configuration.host = self.auth_parser.kube_api_server_ip
+        configuration.host = 'https://' + os.environ['KUBERNETES_SERVICE_HOST']
         configuration.verify_ssl = False  # can be True with ca.crt issued by Kubernetes
         configuration.api_key = {"authorization": "Bearer " + base64.decodebytes(token).decode()}
         client.Configuration.set_default(configuration)
@@ -60,3 +65,35 @@ class ApiInstanceGetter:
         else:
             self.__get_custom_token_auth(custom_token_auth)
         return client.RbacAuthorizationV1Api()
+
+
+    def getApiInstance(self, decodedToken):
+        configuration = client.Configuration()
+        configuration.host = ("https://" + os.environ['KUBERNETES_PORT_443_TCP_ADDR'])
+        configuration.verify_ssl = False
+        configuration.api_key = {"authorization": "Bearer " + base64.decodebytes(str.encode(decodedToken)).decode()}
+        client.Configuration.set_default(configuration)
+        v1 = client.AppsV1Api(client.ApiClient(configuration))
+        return v1
+
+
+#
+# from auth.api_instance_getter import ApiInstanceGetter
+# from resources.deployment_examples import DeploymentExamples
+# from messages.deployment_message import DeploymentMessage
+# from resources.serviceaccount_examples import ServiceaccountExamples
+# import unittest
+#
+#
+# if __name__ == "__main__":
+#     # default
+#     decodedTokenA = 'ZXlKaGJHY2lPaUpTVXpJMU5pSXNJbXRwWkNJNklpSjkuZXlKcGMzTWlPaUpyZFdKbGNtNWxkR1Z6TDNObGNuWnBZMlZoWTJOdmRXNTBJaXdpYTNWaVpYSnVaWFJsY3k1cGJ5OXpaWEoyYVdObFlXTmpiM1Z1ZEM5dVlXMWxjM0JoWTJVaU9pSmtaV1poZFd4MElpd2lhM1ZpWlhKdVpYUmxjeTVwYnk5elpYSjJhV05sWVdOamIzVnVkQzl6WldOeVpYUXVibUZ0WlNJNkltUmxabUYxYkhRdGRHOXJaVzR0TW5vM1pEVWlMQ0pyZFdKbGNtNWxkR1Z6TG1sdkwzTmxjblpwWTJWaFkyTnZkVzUwTDNObGNuWnBZMlV0WVdOamIzVnVkQzV1WVcxbElqb2laR1ZtWVhWc2RDSXNJbXQxWW1WeWJtVjBaWE11YVc4dmMyVnlkbWxqWldGalkyOTFiblF2YzJWeWRtbGpaUzFoWTJOdmRXNTBMblZwWkNJNklqTXpZVFZrWW1VM0xUWTBZemN0TVRGbE9TMDVZMlZoTFRBeVl6aG1abUkyTVdObVl5SXNJbk4xWWlJNkluTjVjM1JsYlRwelpYSjJhV05sWVdOamIzVnVkRHBrWldaaGRXeDBPbVJsWm1GMWJIUWlmUS5nNUkxVXFkVjlHOE1pbUxZTjA4QUhHckJmSXhzQWF3dHMwQlJMNzdRY0tBdVU4RzVISTJEVTlJOHk3cTJpaXJPdkt3OElVZEdvX1lZS3otSDQtRlVuamhXWjJxMTV1cVMtdkR0LTgwTVdvNHJoUHpBY2RYU3Q1Tms5bmRIYVI4XzBjMjNHel8ya3pIX1p3Y1E1d0U0ZVduZHVqNTI0MHhNRjB4TzNraUtzWHRiRzUxLTgyNWVCYVROcGtDMGh3amxoeFpROTJpckNxRFFhR3dILTRkTkIwTFhNSHBNNEJHaXc5d3lXdVZPVkNyV0ZQZWV1UjJpUzQ0YzdkMEVlWWp6TVNuc1hOWEJCMnk5ZVVBaVBVLUZCSnYxa29Rc0hGcU44UjhIT3kwZW1QOHUzb3lwYW5iUFFsSkFoajE0ZU1fNFJVaVFZZFExQ3diSEM3S2RmdWF2LVE='
+#
+#     # alicek106
+#     decodedTokenB = 'ZXlKaGJHY2lPaUpTVXpJMU5pSXNJbXRwWkNJNklpSjkuZXlKcGMzTWlPaUpyZFdKbGNtNWxkR1Z6TDNObGNuWnBZMlZoWTJOdmRXNTBJaXdpYTNWaVpYSnVaWFJsY3k1cGJ5OXpaWEoyYVdObFlXTmpiM1Z1ZEM5dVlXMWxjM0JoWTJVaU9pSmtaV1poZFd4MElpd2lhM1ZpWlhKdVpYUmxjeTVwYnk5elpYSjJhV05sWVdOamIzVnVkQzl6WldOeVpYUXVibUZ0WlNJNkltRnNhV05sYXpFd05pMTBiMnRsYmkxdGVEYzVlQ0lzSW10MVltVnlibVYwWlhNdWFXOHZjMlZ5ZG1salpXRmpZMjkxYm5RdmMyVnlkbWxqWlMxaFkyTnZkVzUwTG01aGJXVWlPaUpoYkdsalpXc3hNRFlpTENKcmRXSmxjbTVsZEdWekxtbHZMM05sY25acFkyVmhZMk52ZFc1MEwzTmxjblpwWTJVdFlXTmpiM1Z1ZEM1MWFXUWlPaUk1T0RJMFlUVXlOeTAyWVRreExURXhaVGt0WW1Zek1pMHdNbU00Wm1aaU5qRmpabU1pTENKemRXSWlPaUp6ZVhOMFpXMDZjMlZ5ZG1salpXRmpZMjkxYm5RNlpHVm1ZWFZzZERwaGJHbGpaV3N4TURZaWZRLlhaQk10Ml9ROVpUdjVVQnRGdG56Q0JpTTBMRkdIQ09yQTQwMFZxVUpMUUdMeHRCMXo5WVZoYTRnYzdEUzl5d2ZFaDRWMDNRZW4yXzRUMExWSXNVbmdOMDRNRmhUOVJtci05TkdpRVVmUVFpUEd5SjNiN0MyZ1NtN1JNNWxHWVZGeGRfcXZybTBHWlotVUpnY054aXdnME9Ea3dOVkEwZ1FSVkhqemhoZUlZOWk0dUhaN1NtRVNMTk0xRk5XYjhBd3NCeWdhaUF2ZlBndVRmc216U3BsLUxwTVZqT3RTcEN3RUs5NEQ2djg0eGFSVzB4SDlORW9VSUZ4a2NxZlpheVVIMFF6d2lWQ0tuVThnWHpPMFd5OVV5aTJrS2FidlRGODZVdkR6SnAxR0tPdWZnRFV1REpiRkV2ZjNjSTFfdnFhQzNBUlFvdHlEejZ4dnJveC0zLTZHQQ=='
+#
+#     clientA = ApiInstanceGetter().getApiInstance(decodedTokenA)
+#     clientB = ApiInstanceGetter().getApiInstance(decodedTokenB)
+#
+#     result, status = DeploymentExamples().create_deployment_sample(
+#         clientB, DeploymentMessage('sample-deployment'))
